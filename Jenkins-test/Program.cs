2bc1e2a1f0df74b786f00db3c625b91a154b881d
Rayson <rayson.dev@gmail.com>
2022-02-22 02:05:33 -0400

    Add project files.

---
+++ Jenkins-test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jenkins_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
